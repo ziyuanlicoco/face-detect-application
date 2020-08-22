ffcb0a711e551eea89546dd1b79f55e5f90053e0
coco <ziyuanli0408@gmail.com>
2020-08-22 02:23:20 -0400

    fix db

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 
-app.get('/', (req,res) =>res.send(database.users))
+app.get('/', (req,res) =>res.send('it is working!'))
 
 app.post('/signin', signin.handleSignIn(db,bcrypt))
 app.post('/register', (req,res) =>{register.handleRegister(req,res,db,bcrypt)})
